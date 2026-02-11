086784b2ffb270962f89769c9c096134e0fc95c6
ferjorosa <ferjorosa@gmail.com>
2026-02-11 20:59:48 +0100

    refactor: group gpt-2 model and block under the same directory

--- tiny_lm/model/architectures/__init__.py
+++ tiny_lm/model/architectures/__init__.py
@@ -1,5 +1,5 @@
 """Full model architectures."""
 
-from .gpt2 import GPT2
+from .gpt2 import GPT2, GPT2Block
 
-__all__ = ["GPT2"]
+__all__ = ["GPT2", "GPT2Block"]
--- tiny_lm/model/blocks/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Transformer block variants."""
-
-from .gpt2_block import GPT2Block
-
-__all__ = ["GPT2Block"]
